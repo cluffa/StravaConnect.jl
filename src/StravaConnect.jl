ae49fb880c4828a62f23aa58d25646192da814ab
cluffa <alexcluff16@gmail.com>
2025-03-07 23:12:53 -0500

    update

--- src/StravaConnect.jl
+++ src/StravaConnect.jl
@@ -5,15 +5,17 @@ using URIs
 using JSON3
 using Dates
 using JLD2
+using DataFrames
+
+using PrecompileTools: @setup_workload, @compile_workload
 
 include("oauth.jl")
 
-export User, refresh_if_needed!, setup_user, get_activity_list, get_activity, 
-    save_user, setup_user_from_file
-
-const VERSION = "1"
+export User, refresh_if_needed!, setup_user, get_activity_list, get_activity
 
 const DATA_DIR = "./data"
+const HIDE = true
+const STREAMKEYS = ("time", "distance", "latlng", "altitude", "velocity_smooth", "heartrate", "cadence", "watts", "temp", "moving", "grade_smooth")
 
 if !isdir(DATA_DIR)
     mkdir(DATA_DIR)
@@ -22,17 +24,17 @@ end
 # conversions
 const METER_TO_MILE = 0.000621371
 const METER_TO_FEET = 3.28084
-c2f(c) = (c * 9/5) + 32
+c2f(c::Number)::Number = (c * 9/5) + 32
 
 mutable struct User
     access_token::String
     refresh_token::String
     expires_at::Int
     expires_in::Int
-    athlete::Dict{Any, Any}
+    athlete::Dict{String, Any}
 end
 
-User() = User("", "", 0, 0, Dict{Any, Any}())
+User() = User("", "", 0, 0, Dict{String, Any}())
 
 """
     authorize!(u::User)::Nothing
@@ -49,7 +51,7 @@ function authorize!(u::User)::Nothing
     u.expires_at = token_info.expires_at
     u.expires_in = token_info.expires_in
 
-    @info "new token expires in $(round(u.expires_in/86400; digits = 1)) days"
+    HIDE || @info "new token expires in $(round(u.expires_in/86400; digits = 1)) days"
 
     return nothing
 end
@@ -67,7 +69,7 @@ function refresh!(u::User)::Nothing
     u.expires_at = new_token.expires_at
     u.expires_in = new_token.expires_in
 
-    @info "new token expires in $(round(u.expires_in/86400; digits = 2)) days"
+    HIDE || @info "new token expires in $(round(u.expires_in/86400; digits = 2)) days"
 
     return nothing
 end
@@ -84,8 +86,6 @@ function refresh_if_needed!(u::User)::Nothing
 
     return nothing
 end
-
-
 
 """
     setup_user(token_file::AbstractString = ".tokens", force_auth::Bool = false)::User
@@ -126,11 +126,60 @@ function setup_user_from_file(user_file::AbstractString)::User
     @assert lowercase(split(user_file, ".")[end]) == "json" "User file must be JSON format"
     user = JSON3.read(user_file, User)
 
-    @info "athlete $(user.athlete["firstname"]) $(user.athlete["lastname"]) ($(user.athlete["id"])) loaded from file"
+    HIDE || @info "athlete $(user.athlete["firstname"]) $(user.athlete["lastname"]) ($(user.athlete["id"])) loaded from file"
 
     refresh_if_needed!(user)
 
     return user
+end
+
+function activites_list_api(access_token::String, page::Int, per_page::Int, mtime::Int; dry_run::Bool = false)
+    if dry_run
+        return HTTP.Response(
+            JSON3.read(read("activites_list_api.json", String))
+        )
+    else 
+        resp = HTTP.get(
+            "https://www.strava.com/api/v3/athlete/activities?page=$page&per_page=$per_page&after=$mtime",
+            headers = Dict("Authorization" => "Bearer $(access_token)")
+        )
+
+        if resp.status == 429
+            @error "Rate limit exceeded"
+            return nothing
+        elseif resp.status != 200
+            @error "Error getting activities"
+            return nothing
+        end
+    
+        return resp
+    end
+end
+
+function activity_api(access_token::String, id::Int; dry_run::Bool = false)::HTTP.Response
+    if dry_run
+        return HTTP.Response(
+            JSON3.read(read("activity_api.json", String))
+        )
+    else
+        response = HTTP.get(
+            "https://www.strava.com/api/v3/activities/$id/streams?keys=$(join(STREAMKEYS, ","))&key_by_type=true",
+            headers = Dict(
+                "Authorization" => "Bearer $(access_token)",
+                "accept" => "application/json"
+            )
+        )
+
+        if response.status == 429;
+            @error "Rate limit exceeded"
+            return nothing
+        elseif response.status != 200
+            @error "Error getting activity"
+            return nothing
+        end
+
+        return response
+    end
 end
 
 """
@@ -138,7 +187,7 @@ end
 
 gets all activities and returns NamedTuple, caches to file
 """
-function get_activity_list(u::User; dataDir = DATA_DIR)::NamedTuple
+function get_activity_list(u::User; dataDir::String = DATA_DIR, dry_run = false)::NamedTuple
     isdir(dataDir) || mkdir(dataDir)
 
     file = jldopen(joinpath(dataDir, "data.jld2"), "a+", compress=true)
@@ -163,26 +212,13 @@ function get_activity_list(u::User; dataDir = DATA_DIR)::NamedTuple
 
     n_cache = length(df.id)
 
-    @info "$(n_cache) activities loaded from cache, getting activities after $(unix2datetime(mtime))"
-
-    header = Dict("Authorization" => "Bearer $(u.access_token)")
+    HIDE || @info "$(n_cache) activities loaded from cache, getting activities after $(unix2datetime(mtime))"
 
     page = 1
     while true
-        body = HTTP.get(
-            "https://www.strava.com/api/v3/athlete/activities?page=$page&per_page=$per_page&after=$mtime",
-            headers = header
-        )
-        
-        if body.status == 429
-            @error "Rate limit exceeded"
-            return nothing
-        elseif body.status != 200
-            @error "Error getting activities"
-            return nothing
-        end
+        resp = activites_list_api(u.access_token, page, per_page, mtime; dry_run)
 
-        body = JSON3.read(body.body)
+        body = JSON3.read(resp.body)
         
         for activity in body
             push!(df.id, activity["id"])
@@ -203,7 +239,7 @@ function get_activity_list(u::User; dataDir = DATA_DIR)::NamedTuple
 
     n_new = length(df.id) - n_cache
 
-    @info "$(n_new) new activities loaded, total $(length(df.id)) activities"
+    HIDE || @info "$(n_new) new activities loaded, total $(length(df.id)) activities"
 
     delete!(file, "activities")
     file["activities"] = df
@@ -220,37 +256,19 @@ end
 
 gets activity using id, returns NamedTuple of vectors
 """
-function get_activity(id, u::User; dataDir = DATA_DIR)::NamedTuple
+function get_activity(id::Int, u::User; dataDir::String = DATA_DIR, dry_run::Bool = false, force_update::Bool = false)::NamedTuple
     refresh_if_needed!(u)
     isdir(dataDir) || mkdir(dataDir)
 
     file = jldopen(joinpath(dataDir, "data.jld2"), "a+", compress=true)
 
-    if haskey(file, "activity_$id")
+    if haskey(file, "activity_$id") && !force_update
         out = file["activity_$id"]
         close(file)
         return out
     end
 
-    streamkeys = ["time", "distance", "latlng", "altitude", "velocity_smooth", "heartrate", "cadence", "watts", "temp", "moving", "grade_smooth"]
-
-    header = Dict(
-        "Authorization" => "Bearer $(u.access_token)",
-        "accept" => "application/json"
-    )
-    
-    response = HTTP.get(
-        "https://www.strava.com/api/v3/activities/$id/streams?keys=$(join(streamkeys, ","))&key_by_type=true",
-        headers = header
-    )
-
-    if response.status == 429;
-        @error "Rate limit exceeded"
-        return nothing
-    elseif response.status != 200
-        @error "Error getting activity"
-        return nothing
-    end
+    response = activity_api(u.access_token, id; dry_run)
 
     body = JSON3.read(response.body)
 
@@ -270,8 +288,8 @@ function get_activity(id, u::User; dataDir = DATA_DIR)::NamedTuple
         moving = Bool[],
         grade_smooth = Float64[]
     )
-
-    for key in streamkeys
+    
+    for key in STREAMKEYS
         if haskey(body, key) && !any(isnothing.(body[key]["data"]))
             if key == "latlng"
                 append!(df[Symbol(key)], Tuple.(body[key]["data"]))
@@ -285,11 +303,32 @@ function get_activity(id, u::User; dataDir = DATA_DIR)::NamedTuple
     append!(df.altitude_ft, df.altitude * METER_TO_FEET)
     append!(df.temp_f, c2f.(df.temp))
 
+    if haskey(file, "activity_$id")
+        delete!(file, "activity_$id")
+    end
+
     file["activity_$id"] = df
 
     close(file)
 
     return df
+end
+
+@setup_workload begin
+    # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
+    # precompile file and potentially make loading faster.
+
+    @compile_workload begin
+        # all calls in this block will be precompiled, regardless of whether
+        # they belong to your package or not (on Julia 1.8 and higher)
+
+        # u = setup_user("data/user.json");
+
+        # activities = get_activity_list(u);
+
+        # x = get_activity(activities.id[end], u);
+
+    end
 end
 
 end  # module
--- test.jl
+++ test.jl
@@ -1,25 +1,9 @@
+using Revise
 using StravaConnect
+using HTTP
 
 u = setup_user("data/user.json");
 
-activities = get_activity_list(u)
+activities = get_activity_list(u);
 
-x = get_activity(activities.id[end], u)
-
-x.heartrate
-
-activities.start_date_local[end]
-
-activities |> keys
-
-minimum(activities.start_date_local)
-
-unique(activities.sport_type)
-
-runs = activities.id[contains.(activities.sport_type, "Run")]
-
-distances = activities.distance_mi[contains.(activities.sport_type, "Run")]
-
-sum(distances)
-
-run_data = [get_activity(id, u) for id in last(runs, 10)]
+x = get_activity(activities.id[1], u; force_update = true);
